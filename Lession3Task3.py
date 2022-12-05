c5ea356f6884310fef9a92676b9f095113522ab3
burmasovanton <looseangel1@gmail.com>
2022-12-05 05:10:55 +0500

    initial2

--- Lession3Task3.py
+++ Lession3Task3.py
@@ -12,8 +12,7 @@ list = [6.1, 4, 6.12, 5.6, 8, 4.01, 6.2]
 variable = 0
 min = decimal.Decimal(list[0]) % 1
 max = decimal.Decimal(list[0]) % 1
-print(min)
-print(max)
+
 for i in list:
     variable = decimal.Decimal(i) % 1
     if variable > max:
@@ -22,4 +21,5 @@ for i in list:
         if variable < min:
             min = variable
 
+print(list)
 print(max - min)
