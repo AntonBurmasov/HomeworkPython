150452f8ba98265e8f122bbf56e7783a6e3c27a7
burmasovanton <looseangel1@gmail.com>
2022-12-16 18:16:16 +0500

    initial

--- Seminar5Task3.py
+++ Seminar5Task3.py
@@ -1,2 +1,38 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+def encode(s):
+    encoding = ""  # сохраняет выходную строку
+
+    i = 0
+    while i < len(s):
+        # подсчитывает количество вхождений символа в индексе `i`
+        count = 1
+
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            count = count + 1
+            i = i + 1
+
+        # добавляет к результату текущий символ и его количество
+        encoding += str(count) + s[i]
+        i = i + 1
+
+    return encoding
+
+def decode(s):
+    decoding = ""
+    count = ''
+    for char in s:
+        if char.isdigit():
+            count += char
+        else:
+            decoding += char * int(count)
+            count = ''
+
+    return decoding
+
+
+string = 'AAAARRRRRRRRRFFFFFFFFFFFFFCDDDDDDDDDDDDD'
+print(string)
+coded_string = (encode(string))
+print(coded_string)
+print(decode(coded_string))
